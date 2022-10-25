1f45a9e788a9b8a5be2afd67eebdca0104e06edc
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-10-25 09:43:47 +0300

    Task-06 fix

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,17 +3,16 @@
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
-const validatedInputRef = document.querySelector('#validation-input');
+const validatedInputRef = document.querySelector("#validation-input");
 
-const validateInputBySymbols = () => { 
-  if (validatedInputRef.value.length < validatedInputRef.dataset.length) {
-    validatedInputRef.classList.remove('valid');
-    validatedInputRef.classList.add('invalid');
+const validateInputBySymbols = () => {
+  if (validatedInputRef.value.length !== +validatedInputRef.dataset.length) {
+    validatedInputRef.classList.remove("valid");
+    validatedInputRef.classList.add("invalid");
+  } else {
+    validatedInputRef.classList.remove("invalid");
+    validatedInputRef.classList.add("valid");
   }
-  else {
-    validatedInputRef.classList.remove('invalid');
-    validatedInputRef.classList.add('valid');
-  }
-}
-  
-validatedInputRef.addEventListener('blur', validateInputBySymbols);
+};
+
+validatedInputRef.addEventListener("blur", validateInputBySymbols);
